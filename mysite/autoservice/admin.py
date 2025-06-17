0a807d457d7cd93ea3dd4c39986dfc370189316f
DonatasNoreika <donoras@gmail.com>
2025-06-17 09:29:37 +0300

    +Užsakyme matomos paslaugų kainos ir sumos (kaina * kiekis)

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -4,7 +4,13 @@ from .models import CarModel, Service, Car, Order, OrderLine
 class OrderLineInLine(admin.TabularInline):
     model = OrderLine
     extra = 0
-    fields = ['service', 'qty']
+    fields = ['service', 'service_price_display', 'qty', 'line_sum']
+    readonly_fields = ['line_sum', 'service_price_display']
+
+    def service_price_display(self, obj):
+        return obj.service.price
+
+    service_price_display.short_description = "Paslaugos Suma"
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['car', 'date', 'total']
