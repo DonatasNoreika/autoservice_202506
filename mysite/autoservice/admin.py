8c619675802da0a9ce4af314d9451637e32d29d8
DonatasNoreika <donoras@gmail.com>
2025-06-17 09:22:55 +0300

    +Užsakymo formoje matoma data ir suma

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -9,6 +9,11 @@ class OrderLineInLine(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['car', 'date', 'total']
     inlines = [OrderLineInLine]
+    readonly_fields = ['date', 'total']
+
+    # fieldsets = (
+    #     ('General', {'fields': ('car', 'date', 'total')}),
+    # )
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ['client_name', 'car_model', 'licence_plate', 'vin_number']
