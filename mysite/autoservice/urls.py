05dfe4becfce2a7f5168e4baed6e836e8e4081a2
DonatasNoreika <donoras@gmail.com>
2025-06-19 08:40:35 +0300

    4 +Paspaudus ant automobilio nuorodos, būtų rodoma detali informacija apie automobilį (savininkas, automobilio modelis, valstybinis numeris, VIN kodas)

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, cars
+from .views import index, cars, car
 
 urlpatterns = [
     path("", index, name="index"),
     path("cars/", cars, name="cars"),
+    path("cars/<int:car_id>", car, name="car"),
 ]
 
