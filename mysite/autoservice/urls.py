f090ad73431eff8ec38cb1a02e2600ad9b98abf5
DonatasNoreika <donoras@gmail.com>
2025-06-19 08:58:26 +0300

    4 +Paspaudus ant užsakymo nuorodos, būtų rodoma detali informacija apie užsakymą.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, cars, car, OrderListView
+from .views import index, cars, car, OrderListView, OrderDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("cars/", cars, name="cars"),
     path("cars/<int:car_id>", car, name="car"),
     path("orders/", OrderListView.as_view(), name="orders"),
+    path("orders/<int:pk>", OrderDetailView.as_view(), name="order"),
 ]
-
