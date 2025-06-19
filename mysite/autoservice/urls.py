8a2f4cb510e992cf2d93a20553c698d08be5ecef
DonatasNoreika <donoras@gmail.com>
2025-06-19 08:47:58 +0300

    4 +Sukurti puslapį (per klasę views faile), pvz. autoservice/uzsakymai, kuriame būtų atvaizduoti visi serviso užsakymai.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, cars, car
+from .views import index, cars, car, OrderListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("cars/", cars, name="cars"),
     path("cars/<int:car_id>", car, name="car"),
+    path("orders/", OrderListView.as_view(), name="orders"),
 ]
 
