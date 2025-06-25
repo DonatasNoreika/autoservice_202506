3f35a2f7154b9ead66a2e608203b2d0ed16688f3
DonatasNoreika <donoras@gmail.com>
2025-06-25 08:09:03 +0300

    6 +Padaryti login puslapį, per kurį leistų prisijungti vartotojui.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+from django.conf.global_settings import LOGIN_REDIRECT_URL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -128,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'autoservice/media')
 
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = "/"
