320f8e0b67ec10f2a75e01ae205c282885d8112e
DonatasNoreika <donoras@gmail.com>
2025-07-01 08:34:08 +0300

    9 +Padaryti, kad prisegta profilio nuotrauka būtų automatiškai sumažinama iki norimo dydžio (pagal dizainą).

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from tinymce.models import HTMLField
+from PIL import Image
 
 # Create your models here.
 class CarModel(models.Model):
@@ -112,3 +113,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} profilis"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.photo.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 3001)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
